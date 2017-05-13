ed6d74ab0390cff6fcc6d93d9d5a568b14bec30f
Evan Shimoniak <FracturedRetina>
2017-05-12 22:35:51 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+var xMin = -5;
+var yMin = -5;
+var xMax = 5;
+var yMax = 5;
+
+var c = document.getElementById("graph");
+var ctx = c.getContext("2d");
+
+var eq = "y/x";
+var LEN = 10;
+
+
+document.getElementsByTagName("input")[0].addEventListener('change', function() {
+	eq = this.value;
+	ctx.clearRect(0, 0, c.width, c.height);
+	drawAxes();
+	drawField();
+});
+
+
+drawAxes();
+drawGrid();
+
+function drawAxes() {
+	//x-axis
+	ctx.beginPath();
+		ctx.moveTo(0, c.height - c.height * Math.abs(yMin / (yMax - yMin)));
+		ctx.lineTo(c.width, c.height - c.height * Math.abs(yMin / (yMax - yMin)));
+	ctx.stroke();
+	//y-axis
+	ctx.beginPath();
+		ctx.moveTo(c.width * Math.abs(xMin / (xMax - xMin)), 0);
+		ctx.lineTo(c.width * Math.abs(xMin / (xMax - xMin)), c.height);
+	ctx.stroke();
+}
+
+function drawField() {
+	for (var y = yMin; y < yMax; y++) {
+		for (var x = xMin; x < xMax; x++) {
+			var scope = {
+				"x": x,
+				"y": y
+			};
+			ctx.beginPath();
+				ctx.moveTo(
+					xToDrawX(x) - LEN / 2,
+					yToDrawY(y) - LEN / 2 * math.eval(eq, scope)
+				);
+				ctx.lineTo(
+					xToDrawX(x) + LEN / 2,
+					yToDrawY(y) + LEN / 2 * math.eval(eq, scope)
+				);
+			ctx.stroke();
+		}
+	}
+}
+
+function drawGrid() {
+	for (var y = yMin; y < yMax; y++) {
+		for (var x = xMin; x < xMax; x++) {
+			if (x != 0 && y != 0) {
+				ctx.fillRect(xToDrawX(x), yToDrawY(y), 2, 2);
+			}
+		}
+	}
+}
+
+function xToDrawX(x) {
+	return c.width * ((xMax - x) / (xMax - xMin));
+}
+
+function yToDrawY(y) {
+	return c.height - c.height * ((yMax - y) / (yMax - yMin));
+}
